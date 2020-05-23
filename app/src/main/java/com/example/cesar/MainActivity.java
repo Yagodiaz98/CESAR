8b468be1b549965fd0f925686504374dec25736b
Yagodiaz98 <Yagodiaz98@users.noreply.github.com>
2020-05-23 13:26:15 +0200

    1. Permisos de internet 2. Botones de registro e inicio de sesión

--- app/src/main/java/com/example/cesar/MainActivity.java
+++ app/src/main/java/com/example/cesar/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.cesar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Views
+    Button mRegisterBtn, mLoginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicialize views
+        mRegisterBtn = findViewById(R.id.register_btn);
+        mLoginBtn = findViewById(R.id.login_btn);
+
+        //handle register button click
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
